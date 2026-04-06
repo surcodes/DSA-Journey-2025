59cb4e88128c80242ac65977c591ccd891a7915f
surcodes <145240222+surcodes@users.noreply.github.com>
2026-04-06 20:52:47 +0530

    Update Largest Element in an Array.cpp

---
+++ Largest Element in an Array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {3, 7, 2, 9, 4};
+    int n = 5;
+
+    int maxElement = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+
+    cout << "Largest element is: " << maxElement;
+
+    return 0;
+}
